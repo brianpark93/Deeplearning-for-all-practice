a1d71e989d16c6bffa428b81a8d22b5c783912e9
brianpark93 <62381208+brianpark93@users.noreply.github.com>
2020-04-02 19:30:10 +0900

    Update lab 04-2.py

--- lab 04/lab 04-2.py
+++ lab 04/lab 04-2.py
@@ -1,14 +1,3 @@
-# Lab 4 Multi-variable linear regression
-import tensorflow as tf
-tf.set_random_seed(777)  # for reproducibility
+import numpy as np
 
-x_data = [[73., 80., 75.],
-          [93., 88., 93.],
-          [89., 91., 90.],
-          [96., 98., 100.],
-          [73., 66., 70.]]
-y_data = [[152.],
-          [185.],
-          [180.],
-          [196.],
-          [142.]]
+xy = np.loadtxt('lab 04-2.csv',delimiter=',',dtype=np.float32)
